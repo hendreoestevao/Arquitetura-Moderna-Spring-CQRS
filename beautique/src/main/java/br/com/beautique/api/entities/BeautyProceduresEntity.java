28fe7a5827eec7ebf550d443258cedc652c1ba3d
hendreo santos <hendreosantos1@outlook.com>
2025-03-23 14:48:08 -0300

    criacao das entity e conexao com o banco

--- beautique/src/main/java/br/com/beautique/api/entities/BeautyProceduresEntity.java
+++ beautique/src/main/java/br/com/beautique/api/entities/BeautyProceduresEntity.java
@@ -1,4 +1,28 @@
 package br.com.beautique.api.entities;
 
-public class BeautyProceduresEntity {
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "beauty_procedures")
+public class BeautyProceduresEntity extends BaseEntity {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
 }
--- beautique/src/main/java/br/com/beautique/api/entities/CustomerEntity.java
+++ beautique/src/main/java/br/com/beautique/api/entities/CustomerEntity.java
@@ -1,4 +1,22 @@
 package br.com.beautique.api.entities;
 
-public class CustomerEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "customer")
+public class CustomerEntity  extends  BaseEntity {
+
+    @Column(nullable = false, length =  100)
+    private String name;
+
+    @Column(nullable = false, length =  100)
+    private String phone;
 }
