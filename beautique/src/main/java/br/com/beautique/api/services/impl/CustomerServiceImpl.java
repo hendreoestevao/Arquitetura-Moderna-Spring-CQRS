3a97b3076a187f0d32630921edfe5237b42963b1
hendreo santos <hendreosantos1@outlook.com>
2025-03-24 22:51:35 -0300

    Model mapper

--- beautique/src/main/java/br/com/beautique/api/services/impl/CustomerServiceImpl.java
+++ beautique/src/main/java/br/com/beautique/api/services/impl/CustomerServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.beautique.api.dtos.CustomerDTO;
 import br.com.beautique.api.entities.CustomerEntity;
 import br.com.beautique.api.repositories.CustomerRepository;
 import br.com.beautique.api.services.CustomerService;
+import br.com.beautique.api.utils.ConverterUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,21 +14,29 @@ public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    private final ConverterUtil<CustomerEntity,CustomerDTO> converterUtil = new ConverterUtil<>(CustomerEntity.class, CustomerDTO.class);
+
     @Override
     public CustomerDTO create(CustomerDTO customerDTO) {
-        CustomerEntity customerEntity = CustomerEntity.builder()
-                .name(customerDTO.getName())
-                .email(customerDTO.getEmail())
-                .phone(customerDTO.getPhone())
-                .build();
-
+        CustomerEntity customerEntity = converterUtil.convertToSource(customerDTO);
         CustomerEntity newCustomerEntity = customerRepository.save(customerEntity);
+        return converterUtil.convertToTarget(newCustomerEntity);
 
-        return CustomerDTO.builder()
-                .id(newCustomerEntity.getId())
-                .name(newCustomerEntity.getName())
-                .email(newCustomerEntity.getEmail())
-                .phone(newCustomerEntity.getPhone())
-                .build();
+
+
+//        CustomerEntity customerEntity = CustomerEntity.builder()
+//                .name(customerDTO.getName())
+//                .email(customerDTO.getEmail())
+//                .phone(customerDTO.getPhone())
+//                .build();
+//
+//        CustomerEntity newCustomerEntity = customerRepository.save(customerEntity);
+//
+//        return CustomerDTO.builder()
+//                .id(newCustomerEntity.getId())
+//                .name(newCustomerEntity.getName())
+//                .email(newCustomerEntity.getEmail())
+//                .phone(newCustomerEntity.getPhone())
+//                .build();
     }
 }
